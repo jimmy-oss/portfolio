e0bc3da286b13bfaf3f0e9a55c067709f3e12027
jimmy-oss <jimmynjonge51@gmail.com>
2021-10-16 21:55:05 +0300

    animate header files

--- script.js
+++ script.js
@@ -8,13 +8,48 @@ const mouseCircleFn = (x,y) =>{
 };
 //End of mouse circle
 //Animated circles
+const circles = document.querySelectorAll(".circle");
+const mainImg = document.querySelector(".main-circle img");
 
+let mX = 0;
+let mY = 0;
+const z = 100;
+
+const animateCircles = (e, x, y) => {
+  if (x < mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `${z}px`;
+    });
+    mainImg.style.left = `${z}px`;
+  } else if (x > mX) {
+    circles.forEach((circle) => {
+      circle.style.left = `-${z}px`;
+    });
+    mainImg.style.left = `-${z}px`;
+  }
+
+  if (y < mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `${z}px`;
+    });
+    mainImg.style.top = `${z}px`;
+  } else if (y > mY) {
+    circles.forEach((circle) => {
+      circle.style.top = `-${z}px`;
+    });
+    mainImg.style.top = `-${z}px`;
+  }
+
+  mX = e.clientX;
+  mY = e.clientY;
+};
 //end of Animated circles
 document.body.addEventListener('mousemove',(e) =>{
    let x = e.clientX;
    let y = e.clientY;
 
    mouseCircleFn(x,y);
+   animateCircles(e,x,y);
 });
 document.body.addEventListener('mouseleave',() =>{
  mouseCircle.style.opacity = "0";
