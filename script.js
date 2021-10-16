8b4842cb108d8c521ff384f929e027e30ac501e8
jimmy-oss <jimmynjonge51@gmail.com>
2021-10-16 17:56:34 +0300

    portfolio files

---
+++ script.js
@@ -0,0 +1,23 @@
+ //Mouse circle
+const mouseCircle = document.querySelector(".mouse-circle");
+const mouseDot = document.querySelector(".mouse-dot");
+const mouseCircleFn = (x,y) =>{
+    mouseCircle.style.cssText = `top: ${y}px;left:${x}px;opacity:1`
+    mouseDot.style.cssText = `top: ${y}px;left:${x}px;opacity:1`
+
+};
+//End of mouse circle
+//Animated circles
+
+//end of Animated circles
+document.body.addEventListener('mousemove',(e) =>{
+   let x = e.clientX;
+   let y = e.clientY;
+
+   mouseCircleFn(x,y);
+});
+document.body.addEventListener('mouseleave',() =>{
+ mouseCircle.style.opacity = "0";
+mouseDot.style.opacity = "0";
+})
+ 
