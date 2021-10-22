745a8cf61ba6deeecc157c79eb0c4d32f81de57a
jimmy-oss <jimmynjonge51@gmail.com>
2021-10-22 13:08:36 +0300

    services

--- script.js
+++ script.js
@@ -168,3 +168,17 @@ projectsBtn.addEventListener('click',(e) =>{
 });
 //End of Projects button
 // End of projects
+
+//section 4
+document.querySelectorAll('.service-btn').forEach(service =>{
+  service.addEventListener('click',e =>{
+  e.preventDefault();
+  const serviceText = service.nextElementSibling
+  serviceText.classList.toggle("change");
+
+  const rightPosition = serviceText.classList.contains
+  ('change') ? `calc(100% - ${getComputedStyle(service.firstElementChild).width})`:0;
+  service.firstElementChild.style.right = rightPosition;
+  });
+});
+//end of section 4
