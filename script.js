a6c94bde176034f592c3c03088805cb57b6db848
jimmy-oss <jimmynjonge51@gmail.com>
2021-11-09 10:03:03 +0300

    form validation

--- script.js
+++ script.js
@@ -395,4 +395,56 @@ setTimeout(() =>{
 },500);
 },3000);
 // end of slideshow
+// form validation
+const form = document.querySelector(".contact-form");
+const username = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+const messages = document.querySelectorAll(".message");
+
+const error = (input, message) => {
+input.nextElementSibling.classList.add("error");
+input.nextElementSibling.textContent = message;
+}
+const success = (input) =>{
+  input.nextElementSibling.classList.remove("error")
+
+}
+const checkRequiredFields = (inputArr) =>{
+inputArr.forEach(input =>{
+if(input.value.trim()  === ""){
+  error(input, `${input.id} is required`);
+} 
+});
+};
+const checkLength = (input, min) => {
+if(input.value.trim().length < min){
+  error(input, `${input.id} must be at least ${min} characters`);
+}else{
+  success(input);
+}
+ 
+};
+
+const checkEmail = (input) => {
+  const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  
+  
+  if(regEx.test(input.value.trim())){
+    success(input)
+  }else{
+    error(input, "Email is not valid")
+  }
+}
+ 
+form.addEventListener("submit", (e) =>{
+   e.preventDefault()
+    checkLength(username,2);
+    checkLength(subject,2);
+    checkLength(message,10);
+    checkEmail(email);
+   checkRequiredFields([username,email,subject,message]);
+})
+// end of form validation
 //end of section 5
